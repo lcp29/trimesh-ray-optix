94f02d0a456800415bc4f2059dc3ca52e79de363
helmholtz <helmholtz@fomal.host>
2024-02-05 10:41:31 +0800

    init commit add setup script and blank README

---
+++ setup.py
@@ -0,0 +1,32 @@
+# helmholtz
+
+import hmesh
+import setuptools
+import os
+
+with open('README.md', 'r') as f:
+    long_descriprion = f.read()
+
+setuptools.setup(
+    name='hmesh',
+    version=hmesh.__version__,
+    author='helmholtz',
+    author_email='helmholtz@fomal.host',
+    description='hmesh - CUDA mesh utilities for trimesh and pytorch',
+    long_description=long_descriprion,
+    long_descriprion_content_type='text/markdown',
+    url='',
+    packages=setuptools.find_packages(),
+    package_data={
+        'hmesh': [
+
+        ]
+    },
+    include_package_data=True,
+    install_requires=['trimesh', 'torch'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.7'
+)
