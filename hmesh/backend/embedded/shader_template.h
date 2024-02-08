088cac29383c8e6b0546dfd086de1491166e626f
helmholtz <helmholtz@fomal.host>
2024-02-08 18:57:19 +0800

    add shader compliation

---
+++ hmesh/backend/embedded/shader_template.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+
+namespace hmesh {
+
+static const size_t shader_length = SHADER_LENGTH;
+
+static const unsigned char shader_code[SHADER_LENGTH] = {
+SHADER_FORMATTED
+};
+
+}
--- setup.py
+++ setup.py
@@ -1,9 +1,61 @@
 
+import os
 import hmesh
+import subprocess
 import setuptools
 
 with open('README.md', 'r') as f:
     long_descriprion = f.read()
+
+SHADER_LOCATION = 'hmesh/backend/shaders.cu'
+SHADER_DIR = os.path.dirname(SHADER_LOCATION)
+SHADER_BASENAME = os.path.basename(SHADER_LOCATION)
+SHADER_WORK_DIR = os.path.join(SHADER_DIR, 'embedded')
+SHADER_IR_PATH = os.path.join(SHADER_WORK_DIR, SHADER_BASENAME[:SHADER_BASENAME.rfind('.')] + '.optixir')
+SHADER_HEADER_PATH = os.path.join(SHADER_WORK_DIR, SHADER_BASENAME[:SHADER_BASENAME.rfind('.')] + '_embedded.h')
+SHADER_TEMPLATE_PATH = os.path.join(SHADER_WORK_DIR, 'shader_template.h')
+
+def compile_and_embed_shaders():
+    optix_include_dir = os.path.join(os.environ['OptiX_INSTALL_DIR'], 'include')
+    # compile shader files
+    if not os.path.exists(SHADER_WORK_DIR):
+        os.makedirs(SHADER_WORK_DIR)
+    # compileer options, see OptiX Programming Guide 6.1
+    compile_command = [
+        'nvcc',
+        '-optix-ir',
+        f'-I{optix_include_dir}',
+        '--use_fast_math',
+        '-m64',
+        '--relocatable-device-code=true',
+        SHADER_LOCATION,
+        '-o',
+        SHADER_IR_PATH
+    ]
+    subprocess.call(compile_command)
+
+    # embed shaders
+    with open(SHADER_IR_PATH, 'rb') as f:
+        shader_hex = f.read().hex()
+    
+    shader_formatted = ''
+    for i in range(0, len(shader_hex), 2):
+        shader_formatted += f'0x{shader_hex[i:i+2]}, ' + ('\n' if i % 16 == 14 else '')
+    
+    shader_formatted = shader_formatted.strip(' ,\n')
+    shader_length = len(shader_hex) // 2
+    
+    with open(SHADER_TEMPLATE_PATH, 'r') as f:
+        shader_template = f.read()
+
+    shader_template = shader_template \
+        .replace('SHADER_LENGTH', str(shader_length)) \
+        .replace('SHADER_FORMATTED', shader_formatted)
+    
+    with open(SHADER_HEADER_PATH, 'w+') as f:
+        f.write(shader_template)
+
+compile_and_embed_shaders()
 
 setuptools.setup(
     name='hmesh',
